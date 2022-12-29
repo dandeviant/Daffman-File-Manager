087457360e9e8366370c2e1aec43f8b4b01a5d24
dandeviant <muhammaddanial63@gmail.com>
2022-12-30 03:28:09 +0800

    export new database sql

--- flask-manager/daffman.sql
+++ flask-manager/daffman.sql
@@ -23,9 +23,12 @@ DROP TABLE IF EXISTS `hash`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `hash` (
-  `filename` varchar(200) DEFAULT NULL,
-  `md5` varchar(50) DEFAULT NULL,
-  `filesize` varchar(45) DEFAULT NULL
+  `filename` varchar(150) DEFAULT NULL,
+  `md5` varchar(45) DEFAULT NULL,
+  `filesize` varchar(45) DEFAULT NULL,
+  `filepasshash` varchar(100) DEFAULT NULL,
+  `filepassraw` varchar(45) DEFAULT NULL,
+  `user_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -35,7 +38,34 @@ CREATE TABLE `hash` (
 
 LOCK TABLES `hash` WRITE;
 /*!40000 ALTER TABLE `hash` DISABLE KEYS */;
+INSERT INTO `hash` VALUES ('uploads/amelia/README.md.aes','7166dc0c21bbde1bcd7c8020a61cfd33','4.18','f0d9991c5e47e0d26a350c1618bd3154cd0f9f2461d3df671a753c393fe7a6a7','amelia',9);
 /*!40000 ALTER TABLE `hash` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `full_name` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','System Administrator'),(2,'daniel','bd3dae5fb91f88a4f0978222dfd58f59a124257cb081486387cbae9df11fb879','Muhammad Danial bin Asmawi'),(9,'amelia','f0d9991c5e47e0d26a350c1618bd3154cd0f9f2461d3df671a753c393fe7a6a7','Amelia Woods'),(10,'hammond','f315d9ecad52f84972fc251230a456bb42ab07ab1a17e17976acb58158723725','John Hammond Jr.');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -47,4 +77,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-23  2:23:23
+-- Dump completed on 2022-12-30  3:27:16
