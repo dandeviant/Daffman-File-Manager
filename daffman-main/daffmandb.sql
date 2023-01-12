82cc94dff4b4a5089cff1b0ac8d832b7a58b3f9c
Muhammad Danial <68473358+dandeviant@users.noreply.github.com>
2023-01-12 11:54:18 +0800

    Update daffmandb.sql

--- daffman-main/daffmandb.sql
+++ daffman-main/daffmandb.sql
@@ -56,7 +56,7 @@ CREATE TABLE `user` (
   `password` varchar(100) DEFAULT NULL,
   `full_name` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
