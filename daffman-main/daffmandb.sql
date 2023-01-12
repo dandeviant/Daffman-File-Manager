39dc889cc414ac07a61aa7188e6c8e9784236fc9
Muhammad Danial <muhammaddanial63@gmail.com>
2023-01-12 11:29:01 +0800

    update db

--- daffman-main/daffmandb.sql
+++ daffman-main/daffmandb.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `flaskmanager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `flaskmanager`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: localhost    Database: flaskmanager
@@ -78,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-12 11:20:00
+-- Dump completed on 2023-01-12 11:28:18
